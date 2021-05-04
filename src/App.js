77a4fbc86153c5a7cb49cfa8d7a08f3557f2d25e
Lostboy561 <donald.poskitt@yahoo.com>
2021-05-04 16:36:51 -0400

    Shop page and preview component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component.jsx';
+import ShopPage from './components/pages/shop/shop.component.jsx';
+
+
+
 
 class App extends React.Component {
 
@@ -11,6 +15,7 @@ class App extends React.Component {
     <div>
     <Switch>
       <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
     </Switch>
     </div>
     )
