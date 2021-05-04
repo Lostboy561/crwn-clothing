6a63ec01e448ea29d05431b2ddc8aa366854609f
Lostboy561 <donald.poskitt@yahoo.com>
2021-05-04 17:07:43 -0400

    Header and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component.jsx';
 import ShopPage from './components/pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 
@@ -13,6 +14,7 @@ class App extends React.Component {
   render() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
